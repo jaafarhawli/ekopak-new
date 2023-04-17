c72e745e7858d4abc81b35cd9a64e52b64fed134
jaafarhawli <jaafarhawle@gmail.com>
2023-04-17 07:46:37 +0300

    client: modified header content

--- client/src/pages/Home/Header.jsx
+++ client/src/pages/Home/Header.jsx
@@ -4,40 +4,44 @@ import images from "../../assets";
 const Header = () => {
   return (
     <section
-      className="bg-primary-100 w-full lg:h-screen h-[900px] flex flex-col lg:flex-row justify-end items-center min-h-[800px]"
+      className="bg-primary-100 w-full lg:h-full flex flex-col lg:flex-row items-center relative lg:py-28 pt-24 2xl:px-24 lg:px-12 sm:px-12 px-4"
       id="header"
     >
-      <div className="hero bg-secondary lg:h-screen lg:min-h-[800px] h-[600px] 2xl:px-24 sm:px-12 px-4 flex flex-col justify-center absolute top-0 left-0 w-full">
-        <h1 className="capitalize sm:text-[60px] text-[40px] font-bold text-navy lg:max-w-[50%]">
+      <div className="hero bg-secondary lg:h-full lg:min-h-[0px] min-h-[40rem] absolute top-0 left-0 w-full"></div>
+
+      <div className="lg:w-[50%] w-full lg:h-full flex-1 lg:mr-4 z-10 flex flex-col justify-center mb-6 lg:mb-0">
+        <h1 className="capitalize xl:text-[60px]  sm:text-[50px] text-[40px] font-bold text-navy">
           Quality supplies for every aspect of your business
         </h1>
-        {/* <p className='text-navy lg:text-[22px] text-[20px] 2xl:text-[28px] max-w-[650px] mt-6'>Quality supplies for every aspect of your business</p> */}
         <h2 className="text-navy lg:text-[20px] 2xl:text-[22px] text-[18px] 2xl:max-w-[650px] lg:max-w-[500px] mt-4">
-   
           Quality is essential in every aspect of business, and our products are
           designed to meet that need. Whether you need industry paper, diaries,
           or restaurant kitchen equipment, we provide top
         </h2>
       </div>
 
-      <div className="w-[50%] h-full"></div>
-
-      <div className="lg:w-[50%] w-full h-full relative">
+      <div className="lg:w-[40%] w-full flex flex-col justify-center items-center">
         <img
           src={images.kitchenIMG}
-          className="absolute right-[50%] translate-x-[50%] xl:w-[350px] sm:w-[300px] w-[250px] max-w-[400px] z-10 rounded-2xl xl:top-[150px] lg:top-[190px] top-[40px] hover:scale-110 hover:z-50 duration-300"
+          className="z-10 rounded-2xl hover:scale-110 hover:z-50 duration-300 translate-y-6"
           alt=""
+          style={{ width: "clamp(310px, 80%, 400px)" }}
         />
-        <img
-          src={images.paperIndustryIMG}
-          className="absolute mx-auto right-[50%] -translate-x-6 2xl:w-[250px] xl:w-[250px] sm:w-[200px] w-[150px] 2xl:top-[400px] lg:top-[380px] top-[200px] rounded-2xl z-20 hover:scale-110 duration-300 hover:z-50"
-          alt=""
-        />
-        <img
-          src={images.diariesIMG}
-          alt=""
-          className="2xl:w-[250px] xl:w-[250px] sm:w-[200px] w-[150px] rounded-2xl absolute mx-auto left-[50%] translate-x-6 2xl:top-[400px] lg:top-[380px] top-[200px] hover:scale-110 duration-300 hover:z-50"
-        />
+
+        <div className="flex justify-center">
+          <img
+            src={images.paperIndustryIMG}
+            className="rounded-2xl z-20 hover:scale-110 duration-300 hover:z-50 -translate-x-2"
+            alt=""
+            style={{ width: "clamp(160px, 45%, 270px)" }}
+          />
+          <img
+            src={images.diariesIMG}
+            alt=""
+            className="w-[45%] rounded-2xl hover:scale-110 duration-300 hover:z-50 translate-x-2"
+            style={{ width: "clamp(160px, 45%, 270px)" }}
+          />
+        </div>
       </div>
     </section>
   );
